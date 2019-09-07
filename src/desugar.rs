69f79ec88ff52012f5fcdfc4398cbad77d1189ff
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-09-07 16:41:03 -0700

    Make desugar cleaner for parser::Let

--- src/desugar.rs
+++ src/desugar.rs
@@ -1,4 +1,4 @@
-use crate::parser::{App, Lam, Let, Node};
+use crate::parser::{App, Lam, Node};
 
 pub fn desugar(mut parser_node: Box<Node>) -> Box<Node> {
     *parser_node = match *parser_node {
@@ -15,20 +15,15 @@ pub fn desugar(mut parser_node: Box<Node>) -> Box<Node> {
             Node::Lam(lam)
         }
 
-        Node::Let(Let {
-            loc,
-            bind,
-            what,
-            in_where,
-        }) => {
-            let what = desugar(what);
-            let in_where = desugar(in_where);
+        Node::Let(let_) => {
+            let what = desugar(let_.what);
+            let in_where = desugar(let_.in_where);
 
             Node::App(App {
-                loc,
+                loc: let_.loc,
                 f: Box::new(Node::Lam(Lam {
-                    loc,
-                    param: bind,
+                    loc: let_.loc,
+                    param: let_.bind,
                     body: in_where,
                 })),
                 arg: what,
